01fed721eb3d18231e92cb09aaa08068edc29086
beamop <beamop@null.net>
2018-03-24 22:08:45 +0100

    Changement du formulaire pour pouvoir entrer une adresse qui va permettre de récuperer la latitude et la longitude grâce à l'API Google Maps puis ajout sur la carte Leaflet

--- src/AppBundle/Form/ObservationType.php
+++ src/AppBundle/Form/ObservationType.php
@@ -5,18 +5,45 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ObservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lieu', Type\TextType::class)
-            ->add('date', Type\TextType::class)
+            ->add('adresse')
+            ->add('zipcode')
+            ->add('ville')
+            ->add('pays')
+            ->add('date', Type\DateType::class)
             ->add('espece', Type\TextType::class)
             ->add('individuals', Type\IntegerType::class)
             ->add('commentaire', Type\TextareaType::class)
         ;
+
+        $builder->addEventListener(
+            FormEvents::SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+
+                $data = $event->getForm()->getData();
+
+                $adresse = $data->getAdresse();
+                $zipcode = $data->getZipcode();
+                $ville = $data->getVille();
+                $pays = $data->getPays();
+                $adresseTot = urlencode($adresse)."+".$zipcode."+".$ville."+".$pays;
+                $apikey = 'AIzaSyB9aZefiyGfTLfqolpIOMny-2Qa3ssDQFE';
+                $geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$adresseTot.'&key='.$apikey);
+                $output = json_decode($geocode);
+                $latitude = $output->results[0]->geometry->location->lat;
+                $longitude = $output->results[0]->geometry->location->lng;
+
+                $data->setLatitude($latitude);
+                $data->setLongitude($longitude);
+            }
+        );
     }
 }
