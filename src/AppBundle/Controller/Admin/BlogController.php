c336c1430960d036fb65a166a61f222e59148eec
Julien <jjabouin@gmail.com>
2018-04-09 17:42:15 +0200

    Interface admin pour : - changer le statut d'un article - modifier le contenu d'un article
    Ajout de l'éditeur CKEditor pour créer un article en HTML sans connaissance du HTML.

--- src/AppBundle/Controller/Admin/BlogController.php
+++ src/AppBundle/Controller/Admin/BlogController.php
@@ -66,8 +66,132 @@ class BlogController extends Controller
         }
 
         return $this->render('nao/admin/blog/ajouter.html.twig', array(
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'bouton' => 'Ajouter'
         ));
+    }
+
+    /**
+     * Modifier article
+     *
+     * @Route("/modifier/{id}", name="admin_post_modifier", requirements={"id": "\d+"})
+     * @Method({"GET","POST"})
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function modifierAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $post = $em
+            ->getRepository(Post::class)
+            ->find($id);
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var UploadedFile $image
+             */
+            $image = $post->getImage();
+
+            if ($image instanceof UploadedFile) {
+                $nom_image = $this->generateUniqueFilename().'.'.$image->guessExtension();
+
+                $image->move(
+                    $this->getParameter('upload_blog_directory'),
+                    $nom_image
+                );
+
+                $post->setImage($nom_image);
+            }
+
+            if ($post->getStatus()===$post::PUBLISHED || $post->getStatus()===$post::FEATURED) {
+                $post->setPublishedAt(new \DateTime());
+            }
+            $em->flush();
+
+            $this->addFlash("success", "L'article a bien été modifié");
+
+            return $this->redirectToRoute('admin_blog');
+        }
+
+        return $this->render('nao/admin/blog/ajouter.html.twig', array(
+            'form' => $form->createView(),
+            'bouton' => 'Modifier'
+        ));
+    }
+
+    /**
+     * Lister les articles
+     *
+     * @Route("/", name="admin_blog")
+     */
+    public function indexAction(Request $request)
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->findBy(
+                array(),
+                array('createdAt' => 'DESC'),
+                10,
+                0
+            );
+
+        return $this->render('nao/admin/blog/index.html.twig', array(
+            'articles' => $articles
+        ));
+    }
+
+    /**
+     * Mettre à jour le statut de l'article
+     *
+     * @Route("/status/{id}/{status}", name="admin_blog_status", requirements={"id": "\d+", "status": "\d+"})
+     * Method({"GET"})
+     */
+    public function statusAction(Request $request, $id, $status)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $post = $em
+            ->getRepository(Post::class)
+            ->find($id);
+
+        $oldStatus = $post->getStatus();
+
+        $post->setStatus($status);
+
+        if ( $oldStatus === null && ($post->getStatus()===$post::PUBLISHED || $post->getStatus()===$post::FEATURED) ) {
+            $post->setPublishedAt(new \DateTime());
+        }
+
+        $em->flush();
+
+        $this->addFlash("success", "Le statut de l'article a bien été mis à jour.");
+
+        return $this->redirectToRoute('admin_blog');
+    }
+
+    /**
+     * @Route("/supprimer/{id}", name="admin_blog_supprimer", requirements={"id": "\d+"})
+     * Method({"GET"})
+     */
+    public function supprimerAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $post = $em
+            ->getRepository(Post::class)
+            ->find($id);
+
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash("success", "L'article a bien été supprimé");
+
+        return $this->redirectToRoute('admin_blog');
     }
 
     /**
